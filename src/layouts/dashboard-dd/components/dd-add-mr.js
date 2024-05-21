e3fd6070e35636146fe1d212c9e144c8f91556ba
Umar Adkhamov <desakondo697@gmail.com>
2024-05-21 16:33:52 +0500

    dd add mr

--- src/layouts/dashboard-dd/components/dd-add-mr.js
+++ src/layouts/dashboard-dd/components/dd-add-mr.js
@@ -1,12 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 // @mui material components
 import Card from "@mui/material/Card";
 import Alert from "@mui/material/Alert";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -20,11 +24,45 @@ import userRoles from "constants/userRoles";
 
 function DeputyDirectorAddMedicalRepresentative() {
   const navigate = useNavigate();
+  const location = useLocation();
   const { accessToken } = useSelector((state) => state.auth);
   const [full_name, setFullname] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [ffm_id, setFieldForceManagerId] = useState("");
+  const [region_manager_id, setRegionalManagerId] = useState("");
+  const [fieldForceManagers, setFieldForceManagers] = useState([]);
+  const [regionalManagers, setRegionalManagers] = useState([]);
   const [message, setMessage] = useState({ color: "", content: "" });
+  const user = location.state || {};
+
+  useEffect(() => {
+    const fetchFieldForceManagers = async () => {
+      console.log(user.username);
+      try {
+        const response = await axios.get(
+          `https://heartly1.uz/common/get-users-by-username?username=${user.username}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        const fieldForceManagers = response.data.filter(
+          (user) => user.status === userRoles.FIELD_FORCE_MANAGER
+        );
+        const regionalManagers = response.data.filter(
+          (user) => user.status === userRoles.REGIONAL_MANAGER
+        );
+        setFieldForceManagers(fieldForceManagers);
+        setRegionalManagers(regionalManagers);
+      } catch (error) {
+        console.error("Failed to fetch users:", error);
+      }
+    };
+
+    fetchFieldForceManagers();
+  }, [accessToken]);
 
   const handleSubmit = async (event) => {
     event.preventDefault(); // Prevent default form submission
@@ -34,30 +72,30 @@ function DeputyDirectorAddMedicalRepresentative() {
       full_name,
       username,
       password,
-      region_id: 0,
-      status: userRoles.PRODUCT_MANAGER
+      ffm_id,
+      product_manager_id: user.id,
+      region_manager_id,
+      region_id: user.region_id,
+      status: userRoles.MEDICAL_REPRESENTATIVE,
     };
 
     try {
       // Call the API with authorization header
-      const response = await axios.post(
-        "https://heartly1.uz/dd/register-for-dd",
-        userData,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
+      const response = await axios.post("https://heartly1.uz/dd/register-for-dd", userData, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
       // Handle a successful response
       setMessage({ color: "success", content: "User successfully registered!" });
 
       // Optional: Redirect after a delay
       setTimeout(() => {
-        navigate("/dd/dashboard");
+        navigate(-1);
       }, 2000);
     } catch (error) {
+      console.log(error);
       // Handle errors gracefully
       setMessage({
         color: "error",
@@ -83,7 +121,7 @@ function DeputyDirectorAddMedicalRepresentative() {
           textAlign="center"
         >
           <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-            Add Product Manager
+            Add Medical Representative
           </MDTypography>
         </MDBox>
         <MDBox pt={4} pb={3} px={3}>
@@ -116,9 +154,45 @@ function DeputyDirectorAddMedicalRepresentative() {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </MDBox>
+            <MDBox mb={2}>
+              <FormControl fullWidth>
+                <InputLabel id="field-force-manager-label">Field Force Manager</InputLabel>
+                <Select
+                  labelId="field-force-manager-label"
+                  value={ffm_id}
+                  label="Field Force Manager"
+                  onChange={(e) => setFieldForceManagerId(e.target.value)}
+                  sx={{ height: "45px" }}
+                >
+                  {fieldForceManagers.map((ffm) => (
+                    <MenuItem key={ffm.id} value={ffm.id}>
+                      {ffm.full_name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </MDBox>
+            <MDBox mb={2}>
+              <FormControl fullWidth>
+                <InputLabel id="regional-manager-label">Regional Manager</InputLabel>
+                <Select
+                  labelId="regional-manager-label"
+                  value={region_manager_id}
+                  label="Regional Manager"
+                  onChange={(e) => setRegionalManagerId(e.target.value)}
+                  sx={{ height: "45px" }}
+                >
+                  {regionalManagers.map((rm) => (
+                    <MenuItem key={rm.id} value={rm.id}>
+                      {rm.full_name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </MDBox>
             <MDBox mt={4} mb={1}>
               <MDButton variant="gradient" color="info" fullWidth type="submit">
-                add
+                Add
               </MDButton>
             </MDBox>
           </MDBox>
