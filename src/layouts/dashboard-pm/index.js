c4d60699afad1ca256995221f3bc23093244cf0d
Umar Adkhamov <desakondo697@gmail.com>
2024-07-02 14:25:07 +0500

    Remove everything from pm dashboard

--- src/layouts/dashboard-pm/index.js
+++ src/layouts/dashboard-pm/index.js
@@ -1,32 +1,6 @@
-// @mui material components
-import Grid from "@mui/material/Grid";
-
-// Material Dashboard 2 React components
-import MDBox from "components/MDBox";
-
-// Material Dashboard 2 React example components
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-
-import Users from "layouts/user-table";
-import { Outlet } from "react-router-dom";
-
 // eslint-disable-next-line react/prop-types
 function ProductManagerDashboard() {
-  return (
-    <DashboardLayout>
-      {/* <DashboardNavbar /> */}
-      <MDBox py={3}>
-        <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={12}>
-              <Users />
-            </Grid>
-          </Grid>
-        </MDBox>
-      </MDBox>
-      <Outlet />
-    </DashboardLayout>
-  );
+  return <></>;
 }
 
 export default ProductManagerDashboard;
