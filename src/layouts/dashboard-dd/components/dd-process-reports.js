e4dc3ee3275afc4e87397a5e61f6982275c1d6e3
Umar Adkhamov <desakondo697@gmail.com>
2024-07-03 15:11:00 +0500

    Fix process report download

--- src/layouts/dashboard-dd/components/dd-process-reports.js
+++ src/layouts/dashboard-dd/components/dd-process-reports.js
@@ -1,34 +1,107 @@
-// @mui material components
-import Grid from "@mui/material/Grid";
-
-// Material Dashboard 2 React components
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
 import MDBox from "components/MDBox";
-
-// Material Dashboard 2 React example components
+import { FormControl, InputLabel, MenuItem, Select, Button, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import axios from "axios";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
-// Dashboard components
-import { Outlet, useNavigate } from "react-router-dom";
-import DeputyDirectorProcessReportTable from "../table/process-report-table";
-// import DeputyDirectorTable from "../table";
+const months = [
+  { name: "Январь", value: 1 },
+  { name: "Февраль", value: 2 },
+  { name: "Март", value: 3 },
+  { name: "Апрель", value: 4 },
+  { name: "Май", value: 5 },
+  { name: "Июнь", value: 6 },
+  { name: "Июль", value: 7 },
+  { name: "Август", value: 8 },
+  { name: "Сентябрь", value: 9 },
+  { name: "Октябрь", value: 10 },
+  { name: "Ноябрь", value: 11 },
+  { name: "Декабрь", value: 12 },
+];
 
-// eslint-disable-next-line react/prop-types
-function DeputyDirectorProcessReports({ children }) {
-  const navigate = useNavigate();
+function DeputyDirectorProcessReport() {
+  const currentMonth = new Date().getMonth() + 1; // Get current month (1-12)
+  const [month, setMonth] = useState(currentMonth);
+  const accessToken = useSelector((state) => state.auth.accessToken);
+
+  const handleMonthChange = (event) => {
+    setMonth(event.target.value);
+  };
+
+  const downloadReport = async () => {
+    const monthName = months.find((m) => m.value === month).name;
+    try {
+      const response = await axios.get(
+        `https://it-club.uz/dd/get-proccess-report-ecxel?month=${month}`,
+        {
+          responseType: "blob",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      const filename = `report_${monthName}.xlsx`;
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", filename);
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    } catch (error) {
+      console.error("Download error", error);
+    }
+  };
+
   return (
     <DashboardLayout>
-      <MDBox py={3}>
-        <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={12}>
-              <DeputyDirectorProcessReportTable />
-            </Grid>
-          </Grid>
+      <Card>
+        <MDBox
+          display="flex"
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          p={6}
+          width="100%"
+          gap="20px"
+        >
+          <Typography variant="h1" component="h1">
+            Отчеты
+          </Typography>
+          <MDBox display="flex" gap="20px" alignItems="center">
+            <FormControl variant="outlined" sx={{ minWidth: 200 }}>
+              <InputLabel id="month-select-label">Месяц</InputLabel>
+              <Select
+                labelId="month-select-label"
+                value={month}
+                onChange={handleMonthChange}
+                label="Месяц"
+                sx={{ height: 45 }}
+              >
+                {months.map((month) => (
+                  <MenuItem key={month.value} value={month.value}>
+                    {month.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ color: "white" }}
+              onClick={downloadReport}
+            >
+              Получить отчет на {months.find((m) => m.value === month).name}
+            </Button>
+          </MDBox>
         </MDBox>
-      </MDBox>
-      <Outlet />
+      </Card>
     </DashboardLayout>
   );
 }
 
-export default DeputyDirectorProcessReports;
+export default DeputyDirectorProcessReport;
